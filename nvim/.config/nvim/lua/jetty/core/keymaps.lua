432239a8c4823584cfa2b5a857fc77e5315b3ca3
jettywing <125429415+jettywing@users.noreply.github.com>
2024-09-23 11:45:22 -0400

    esc to noh

--- nvim/.config/nvim/lua/jetty/core/keymaps.lua
+++ nvim/.config/nvim/lua/jetty/core/keymaps.lua
@@ -12,6 +12,9 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- unhighlight
+vim.keymap.set("n", "<Esc>", "<cmd>:noh<CR>")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
