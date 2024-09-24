d0b561031c00f21158d8aa023bef4094539d8274
jettywing <125429415+jettywing@users.noreply.github.com>
2024-09-24 01:28:26 -0400

    using aerospace now

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,6 +7,18 @@ local config = wezterm.config_builder()
 config.window_background_opacity = 0.67
 config.macos_window_background_blur = 8
 
+-- will enable if I can fix ugly border overlap
+-- config.window_frame = {
+-- 	border_left_width = "0.5cell",
+-- 	border_right_width = "0.5cell",
+-- 	border_bottom_height = "0.25cell",
+-- 	border_top_height = "0.25cell",
+-- 	border_left_color = "#093F6A",
+-- 	border_right_color = "#093F6A",
+-- 	border_bottom_color = "#093F6A",
+-- 	border_top_color = "#093F6A",
+-- }
+
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
